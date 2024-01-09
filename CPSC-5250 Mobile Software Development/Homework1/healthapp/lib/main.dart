e2b89371192106ae5d31acd1d43a884397ce8371
Kapish-patel <kpatelkapish@gmail.com>
2024-01-08 22:19:44 -0800

    3 page view completed

--- CPSC-5250 Mobile Software Development/Homework1/healthapp/lib/main.dart
+++ CPSC-5250 Mobile Software Development/Homework1/healthapp/lib/main.dart
@@ -13,113 +13,221 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Healthfy',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyanAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Healthfy"),
+        backgroundColor: Colors.cyanAccent,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: const Pagination(),
+    );
+  }
+}
+
+class Pagination extends StatelessWidget {
+  const Pagination({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController();
+    return PageView(
+      controller: controller,
+      children: const <Widget>[
+        EmotionRecorder(),
+        DietRecorder(),
+        WorkoutRecorder()
+      ],
+    );
+  }
+}
+
+class EmotionRecorder extends StatelessWidget {
+  const EmotionRecorder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: <Widget>[
+        EmojiSection(),
+        EmotionListSection()
+      ],
+    );
+  }
+}
+
+class EmojiSection extends StatefulWidget {
+  const EmojiSection({super.key});
+
+
+  @override
+  EmojiGridSectionState createState() {
+    return EmojiGridSectionState();
+  }
+}
+
+class EmojiGridSectionState extends State<EmojiSection>{
+  final Map<String, String> emojiExpressions = {
+    "😊": "Happy",
+    "😢": "Sad",
+    "😂": "Laughing",
+    "😍": "In Love",
+    "😎": "Cool",
+    "😐": "Neutral",
+    "😡": "Angry",
+    "😴": "Sleepy",
+    "🥳": "Celebrating",
+    "🤔": "Thinking",
+    "😇": "Angel",
+    "😈": "Devilish",
+    "🤗": "Hugging",
+    "🤢": "Nauseated",
+    "🤐": "Zipped Lips",
+    "😪": "Sleepy Face",
+    "😳": "Blushing",
+    "🙄": "Face with Rolling Eyes",
+    "😷": "Face with Medical Mask",
+    "🥺": "Pleading Face",
+    "🤯": "Exploding Head",
+    "😸": "Grinning Cat Face",
+    "😱": "Surprised",
+    "🤩": "Starstruck",
+  };
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Wrap(
+        spacing: 8.0, // h space
+        runSpacing: 8.0, // v space
+        children: List<Widget>.from(
+          emojiExpressions.keys.map((emoji) => DisplayCustomEmoji(data: emoji))
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class EmotionListSection extends StatefulWidget{
+  const EmotionListSection({super.key});
+
+  @override
+  EmotionListSectionState createState() {
+    return EmotionListSectionState();
+  }
+}
+
+class EmotionListSectionState extends State<EmotionListSection> {
+  final String data = "this is Emotion list section";
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(data);
+  }
+}
+
+
+class DisplayCustomEmoji extends StatelessWidget {
+  final String data;
+
+  const DisplayCustomEmoji({
+    required this.data,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Text(data, style: const TextStyle(fontSize: 30.0)));
+  }
+}
+
+class DietRecorder extends StatefulWidget {
+  const DietRecorder({super.key});
+
+  @override
+  DietRecorderState createState() {
+    return DietRecorderState();
+  }
+}
+
+class DietRecorderState extends State<DietRecorder> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController mealNameController = TextEditingController();
+  final TextEditingController mealQuantityController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: mealNameController,
+            decoration: const InputDecoration(labelText: 'Meal Name'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter a Valid Meal Name';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: mealQuantityController,
+            decoration: const InputDecoration(labelText: 'Meal Quantity'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter a Valid Meal Quantity';
+              }
+              // You can add more validation logic for quantity if needed
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // Process the inputs, for example, save to a database
+                String mealName = mealNameController.text;
+                String mealQuantity = mealQuantityController.text;
+                print('Meal name: $mealName');
+                print('MealQuantity: $mealQuantity');
+
+                // Clear the input field
+                mealNameController.clear();
+                mealQuantityController.clear();
+
+                // Notify the user using snackbar notification
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Successfully submitted Data!'),
+                ));
+              }
+            },
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WorkoutRecorder extends StatelessWidget {
+  const WorkoutRecorder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Workout Recorder");
   }
 }

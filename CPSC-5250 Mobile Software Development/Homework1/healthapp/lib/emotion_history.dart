a6da99b6d7fac884f0869d35c660663670497c9c
Kapish-patel <kpatelkapish@gmail.com>
2024-01-23 15:26:05 -0800

    emotion history feature done

--- CPSC-5250 Mobile Software Development/Homework1/healthapp/lib/emotion_history.dart
+++ CPSC-5250 Mobile Software Development/Homework1/healthapp/lib/emotion_history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:healthapp/data_classes/emotion_recorder_event.dart';
+import 'package:healthapp/old/card_list.dart';
 
 class EmotionHistory extends StatefulWidget{
   final List<EmotionRecorderEvent> emotionRecorderEventsHistory;
@@ -20,11 +21,6 @@ class _EmotionHistoryState extends State<EmotionHistory> {
   void initState() {
     super.initState();
     emotionRecorderEventsHistory = widget.emotionRecorderEventsHistory;
-
-    if(_scrollController.hasClients){
-      final position = _scrollController.position.minScrollExtent;
-      _scrollController.jumpTo(position);
-    }
   }
 
 // start working here for auto scrolling and adding new items to the list
@@ -34,16 +30,27 @@ class _EmotionHistoryState extends State<EmotionHistory> {
       height: 300.0,
       child: ListView.builder(
         controller: _scrollController,
-        reverse: true,
+        reverse: false,
         itemCount: emotionRecorderEventsHistory.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: Text(emotionRecorderEventsHistory[index].emoji),
-            title: Text(emotionRecorderEventsHistory[index].emotionName),
-            subtitle: Text(emotionRecorderEventsHistory[index].dateTime.toString()),
-          );
+          return CardList(
+            emoji: emotionRecorderEventsHistory[index].emoji,
+            value: [emotionRecorderEventsHistory[index].emotionName, emotionRecorderEventsHistory[index].dateTime.toString()]);
         },
-      )
+      ),
     );
+  }
+
+  // This is the part that makes the list auto scroll to the bottom
+  @override
+  void didUpdateWidget(EmotionHistory oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
   }
 }
